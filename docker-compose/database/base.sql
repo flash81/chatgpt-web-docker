ddc4f01ee76188914aaa9946447d19c9d10c39df
yefengli <lyf@ddpkcc.com>
2023-05-28 15:35:11 +0800

    sql

--- docker-compose/database/base.sql
+++ docker-compose/database/base.sql
@@ -1,21 +1,44 @@
 /*
  Navicat MySQL Data Transfer
 
- Source Server         : beargpt
+ Source Server         : sfsdf
  Source Server Type    : MySQL
- Source Server Version : 50742
- Source Host           : 8.219.150.254
+ Source Server Version : 50736
+ Source Host           : localhost
  Source Database       : base
 
  Target Server Type    : MySQL
- Target Server Version : 50742
+ Target Server Version : 50736
  File Encoding         : utf-8
 
- Date: 05/16/2023 10:25:03 AM
+ Date: 05/28/2023 15:34:20 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `admin_config`
+-- ----------------------------
+DROP TABLE IF EXISTS `admin_config`;
+CREATE TABLE `admin_config` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '类型0文本1图片2富文本',
+  `name` varchar(45) NOT NULL,
+  `value` text NOT NULL,
+  `description` varchar(45) NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index2` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+--  Records of `admin_config`
+-- ----------------------------
+BEGIN;
+INSERT INTO `admin_config` VALUES ('3', '0', 'free_day', '1', '免费使用天数', '2023-04-24 21:49:36', '2023-05-17 17:59:31');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `user`
@@ -38,7 +61,7 @@ CREATE TABLE `user` (
   `deleted_at` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `token_idx` (`remember_token`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='用户表';
 
 -- ----------------------------
 --  Table structure for `user_chat`
@@ -54,18 +77,6 @@ CREATE TABLE `user_chat` (
   `deleted_at` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fk_order_user1_idx` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=302 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='订单';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC COMMENT='订单';
 
-DROP TABLE IF EXISTS `admin_config`;
-CREATE TABLE `admin_config` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '类型0文本1图片2富文本',
-  `name` varchar(45) NOT NULL,
-  `value` text NOT NULL,
-  `description` varchar(45) NOT NULL DEFAULT '',
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `index2` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 SET FOREIGN_KEY_CHECKS = 1;
